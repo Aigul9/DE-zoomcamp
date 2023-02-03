ea8bde998bf452b842c0d34c811fd050e0633473
Aigul <aigul.sibgatulli@mail.ru>
2023-02-03 21:20:33 +0300

    save file from web to yandex cloud

---
+++ web_to_cloud.py
@@ -0,0 +1,48 @@
+import boto3
+import wget
+
+from decouple import config
+from prefect import flow, task
+
+
+@task(log_prints=True, retries=3)
+def fetch(dataset_url: str, path: str) -> None:
+    """Save parquet file from web to a local folder"""
+    wget.download(dataset_url, out=path)
+
+
+@task
+def write_to_yandex_cloud(path, bucket_name):
+    session = boto3.session.Session()
+    s3 = session.client(
+        service_name='s3',
+        endpoint_url='https://storage.yandexcloud.net',
+        aws_access_key_id=config('AWS_ACCESS_KEY'),
+        aws_secret_access_key=config('AWS_SECRET_KEY')
+    )
+
+    try:
+        s3.create_bucket(Bucket=bucket_name)
+    except s3.exceptions.BucketAlreadyOwnedByYou:
+        pass
+    s3.upload_file(path, bucket_name, path)
+
+
+@flow
+def main_flow():
+    """The main ETL function"""
+    color = 'green'
+    year = 2020
+    month = 1
+    dataset_file = f'{color}_tripdata_{year}-{month:02}.parquet'
+    dataset_url = f'https://d37ci6vzurychx.cloudfront.net/trip-data/{dataset_file}'
+    path = f'data/{dataset_file}'
+
+    fetch(dataset_url, path)
+
+    bucket = 'zoomcamp-bucket'
+    write_to_yandex_cloud(path, bucket)
+
+
+if __name__ == '__main__':
+    main_flow()
